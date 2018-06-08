588a599ffe9506c85543e9c731ee95827b290b81
Emanuel Virca <emanuel.virca@gmail.com>
2018-06-08 07:15:27 +0300

    user should see resources in a masonry like style

--- src/app/resources/resources.module.ts
+++ src/app/resources/resources.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatCardModule, MatIconModule } from '@angular/material';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxMasonryModule } from 'ngx-masonry';
 
 import { ResourcesRoutingModule } from './resources-routing.module';
 import { ResourcesListComponent } from './resources-list/resources-list.component';
@@ -12,6 +13,7 @@ import { ResourceCardComponent } from './resource-card/resource-card.component';
         CommonModule,
         ResourcesRoutingModule,
         InfiniteScrollModule,
+        NgxMasonryModule,
         MatButtonModule,
         MatCardModule,
         MatIconModule,
