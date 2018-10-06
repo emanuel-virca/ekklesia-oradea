aa8748a5495d39b986d75cb14e4e440504156c18
Emanuel Virca <emanuel.virca@gmail.com>
2018-10-06 17:31:59 +0300

    fix seconds beeing delayed with almost 900ms

--- src/app/shared/pipes/seconds-to-time/seconds-to-time.pipe.ts
+++ src/app/shared/pipes/seconds-to-time/seconds-to-time.pipe.ts
@@ -12,9 +12,10 @@ export class SecondsToTimePipe implements PipeTransform {
     return this.convertSecondsToTime(value);
   }
 
-  private convertSecondsToTime(currentTime) {
-    const minutes = Math.floor(currentTime / 60),
-      seconds = Math.floor(currentTime % 60);
+  private convertSecondsToTime(duration) {
+    const roundedSeconds = Math.round(duration);
+    const minutes = Math.floor(roundedSeconds / 60),
+      seconds = Math.floor(roundedSeconds % 60);
 
     return this.formatTime(seconds, minutes);
   }
