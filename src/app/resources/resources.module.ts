a5e39e4aad7ac8c53b034fd8915bb85643ea5301
Emanuel Virca <emanuel.virca@gmail.com>
2019-01-24 16:10:34 +0200

    feat(resource-details): user should be able to see audi-resource details

--- src/app/resources/resources.module.ts
+++ src/app/resources/resources.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { NgxMasonryModule } from 'ngx-masonry';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { SharedModule } from '../shared/shared.module';
 import { ResourcesRoutingModule } from './resources-routing.module';
 import { ResourcesListComponent } from './resources-list/resources-list.component';
 import { ResourceCardComponent } from './resource-card/resource-card.component';
 import { ResourcesSearchResultsComponent } from './resources-search-results/resources-search-results.component';
+import { ResourceDetailsComponent } from './resource-details/resource-details.component';
+import { ResourceDetailsShellComponent } from './containers/resource-details-shell/resource-details-shell.component';
+
+// NgRx
+import { resourceReducer } from './state/resource.reducers';
+import { ResourceEffects } from './state/resource.effects';
+
 
 @NgModule({
     imports: [
@@ -14,11 +23,15 @@ import { ResourcesSearchResultsComponent } from './resources-search-results/reso
         InfiniteScrollModule,
         NgxMasonryModule,
         SharedModule,
+        StoreModule.forFeature('resources', resourceReducer),
+        EffectsModule.forFeature([ResourceEffects]),
     ],
     declarations: [
         ResourcesListComponent,
         ResourceCardComponent,
         ResourcesSearchResultsComponent,
+        ResourceDetailsComponent,
+        ResourceDetailsShellComponent,
     ],
     exports: [],
     providers: []
---
+++ src/app/resources/state/resource.effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+// NgRx
+import { Action } from '@ngrx/store';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap, catchError, map } from 'rxjs/operators';
+
+import * as resourceActions from './resource.actions';
+import { ResourceService } from 'src/app/shared/services/resource/resource.service';
+
+@Injectable()
+export class ResourceEffects {
+  constructor(
+    private actions$: Actions,
+    private resourceService: ResourceService,
+  ) { }
+
+  @Effect()
+  loadResources$: Observable<Action> = this.actions$.pipe(
+    ofType(resourceActions.LOAD_RESOURCE),
+    map((action: resourceActions.LoadResource) => action.payload),
+    switchMap(resourceId =>
+      this.resourceService.get(resourceId).pipe(
+        map(resource => new resourceActions.LoadResourceSuccess(resource)),
+        catchError(err => of(new resourceActions.LoadResourceFail(err)))
+      )
+    )
+  );
+}
