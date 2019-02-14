a0c949329d3ee340597be676bf78ffe22234719e
Emanuel Virca <emanuel.virca@gmail.com>
2019-02-14 09:34:34 +0200

    feat(flex-layout): use angular flex to compute grid

--- src/app/resources/resources-list/resources-list.component.ts
+++ src/app/resources/resources-list/resources-list.component.ts
@@ -5,82 +5,113 @@ import { LoaderService } from '../../core/services/loader/loader.service';
 import { ResourceService } from '../../shared/services/resource/resource.service';
 
 @Component({
-    selector: 'app-resources-list',
-    templateUrl: './resources-list.component.html',
-    styleUrls: ['./resources-list.component.scss']
+  selector: 'app-resources-list',
+  templateUrl: './resources-list.component.html',
+  styleUrls: ['./resources-list.component.scss'],
 })
 export class ResourcesListComponent implements OnInit, AfterViewInit {
+  @ViewChild('masonryItemSizer') masonryItemSizer: ElementRef;
 
-    @ViewChild('masonryItemSizer') masonryItemSizer: ElementRef;
+  resources: Resource[] = new Array<Resource>();
+  lastVisible: Resource;
+  pageSize = 20;
+  loading = false;
+  thereIsMore = true;
+  cardWidth: number;
+  viewInitalized = false;
+  updateMasonryLayout = false;
+  masonryOptions: any = {
+    transitionDuration: '0',
+    horizontalOrder: true,
+    columnWidth: '.masonry-item-sizer',
+    gutter: '.gutter-sizer',
+    percentPosition: true,
+    itemSelector: '.masonry-item',
+    resize: false,
+  };
+  masonryStyleElement: HTMLStyleElement;
+  masonryGutterSize = 24;
 
-    resources: Resource[] = new Array<Resource>();
-    lastVisible: Resource;
-    pageSize = 20;
-    loading = false;
-    thereIsMore = true;
-    cardWidth: number;
-    viewInitalized = false;
-    updateMasonryLayout = false;
-    masonryOptions: any = {
-        transitionDuration: '0',
-        horizontalOrder: true,
-        columnWidth: '.masonry-item-sizer',
-        gutter: '.gutter-sizer',
-        percentPosition: true,
-        itemSelector: '.masonry-item',
-        resize: false
-    };
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.cardWidth = this.getMasonryItemSize();
+    this.setStyle();
+    setTimeout(() => (this.updateMasonryLayout = !this.updateMasonryLayout), 100);
+  }
 
-    @HostListener('window:resize', ['$event'])
-    onResize(event) {
-        this.cardWidth = this.getMasonryItemSize();
-        setTimeout(() => this.updateMasonryLayout = !this.updateMasonryLayout, 100);
-    }
+  constructor(private resourceService: ResourceService, private loaderService: LoaderService) {}
 
-    constructor(
-        private resourceService: ResourceService,
-        private loaderService: LoaderService,
-    ) {
+  ngOnInit() {
+    this.masonryStyleElement = this.createStyleTag();
+    this.getNextResources();
+  }
 
-    }
+  ngAfterViewInit() {
+    this.cardWidth = this.getMasonryItemSize();
+    this.setStyle();
 
-    ngOnInit() {
-        this.getNextResources();
-    }
+    setTimeout(() => (this.viewInitalized = true), 100);
+  }
 
-    ngAfterViewInit() {
-        this.cardWidth = this.getMasonryItemSize();
-        console.log('cardWidth', this.cardWidth);
-        setTimeout(() => this.viewInitalized = true);
-    }
+  createStyleTag() {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.setAttribute('id', 'masonry-style');
+    return document.head.appendChild(style);
+  }
 
-    onScroll() {
-        if (!this.loading && this.thereIsMore) {
-            this.getNextResources();
+  setStyle() {
+    const cssRules = `
+        .masonry-item,
+        .masonry-item-sizer {
+            width:  ${this.cardWidth}px;
+            margin-bottom: ${this.masonryGutterSize}px
         }
-    }
 
-    private getNextResources() {
-        // TODO move loader
-        this.loading = true;
-        this.loaderService.show();
+        .gutter-sizer {
+            width: ${this.masonryGutterSize}px;
+        }
 
-        this.resourceService.query(this.pageSize, this.lastVisible, 'desc').subscribe(
-            (items: Resource[]) => {
-                this.resources = this.resources.concat(items);
-                this.lastVisible = items[items.length - 1];
-                if (items.length < this.pageSize) {
-                    this.thereIsMore = false;
-                }
-                this.loading = false;
-                this.loaderService.hide();
-            },
-            err => { this.loading = false; this.loaderService.hide(); }
-        );
-    }
+        .masonry-grid {
+            margin: ${this.masonryGutterSize}px;
+        }
+    `;
 
-    private getMasonryItemSize(): number {
-        if (!this.masonryItemSizer) { return 0; }
-        return this.masonryItemSizer.nativeElement.getBoundingClientRect().width;
+    this.masonryStyleElement.innerHTML = cssRules;
+  }
+
+  onScroll() {
+    if (!this.loading && this.thereIsMore) {
+      this.getNextResources();
     }
+  }
+
+  private getNextResources() {
+    // TODO move loader
+    this.loading = true;
+    this.loaderService.show();
+
+    this.resourceService.query(this.pageSize, this.lastVisible, 'desc').subscribe(
+      (items: Resource[]) => {
+        this.resources = this.resources.concat(items);
+        this.lastVisible = items[items.length - 1];
+        if (items.length < this.pageSize) {
+          this.thereIsMore = false;
+        }
+        this.loading = false;
+        this.loaderService.hide();
+      },
+      err => {
+        this.loading = false;
+        this.loaderService.hide();
+      }
+    );
+  }
+
+  private getMasonryItemSize(): number {
+    if (!this.masonryItemSizer) {
+      return 0;
+    }
+    return this.masonryItemSizer.nativeElement.getBoundingClientRect().width;
+  }
 }
