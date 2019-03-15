45d895cd8052604730d4bdfbb4c29d62cc93122c
Emanuel Virca <emanuel.virca@gmail.com>
2019-03-15 22:49:11 +0200

    feat(search): add clear button, some style work

--- src/app/core/services/search/search.service.ts
+++ src/app/core/services/search/search.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
 import * as algoliasearchProxy from 'algoliasearch';
-import { Subject } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
 import { ResourceSearchResult } from '../../../shared/components/resources-search/resource-search-result.model';
-import { SearchState } from '../../models/search-state';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SearchService {
-
   private algoliaIndex: algoliasearchProxy.Index;
-  private searchVisibilitySubject = new Subject<SearchState>();
-
-  public searchState = this.searchVisibilitySubject.asObservable();
 
   constructor() {
     const client = algoliasearchProxy(environment.algolia.applicationId, environment.algolia.apiKey);
@@ -22,16 +16,8 @@ export class SearchService {
   }
 
   public searchResourcesAsync(search: string, pageNo: number, pageSize: number): Promise<ResourceSearchResult[]> {
-    return this.algoliaIndex.search({ query: search, page: pageNo, hitsPerPage: pageSize }).then(function (responses) {
+    return this.algoliaIndex.search({ query: search, page: pageNo, hitsPerPage: pageSize }).then(function(responses) {
       return responses.hits;
     });
-  }
-
-  show() {
-      this.searchVisibilitySubject.next(<SearchState>{ show: true });
-  }
-
-  hide() {
-      this.searchVisibilitySubject.next(<SearchState>{ show: false });
   }
 }
--- src/app/shared/components/resources-search/resources-search.component.ts
+++ src/app/shared/components/resources-search/resources-search.component.ts
@@ -1,39 +1,31 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatAutocompleteSelectedEvent, MatAutocompleteTrigger } from '@angular/material';
-import { Subscription } from 'rxjs';
 
 import { ResourceSearchResult } from './resource-search-result.model';
 import { SearchService } from 'src/app/core/services/search/search.service';
-import { SearchState } from 'src/app/core/models/search-state';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-resources-search',
   templateUrl: './resources-search.component.html',
   styleUrls: ['./resources-search.component.scss'],
 })
-export class ResourcesSearchComponent implements OnInit, AfterViewInit, OnDestroy {
-  searchCtrl: FormControl;
+export class ResourcesSearchComponent implements OnInit, AfterViewInit {
+  searchCtrl: FormControl = new FormControl();
   searchResults: Array<ResourceSearchResult>;
   showAutocompletePanel: boolean;
-  searchVisibilitySubscription: Subscription;
   showSearch = false;
 
   @ViewChild('matAutocompleteTrigger', { read: MatAutocompleteTrigger }) matAutocompleteTrigger: MatAutocompleteTrigger;
   @ViewChild('searchInput') searchInput: ElementRef<HTMLInputElement>;
 
-  constructor(private searchService: SearchService, private router: Router) {
-    this.searchCtrl = new FormControl();
-  }
+  constructor(private searchService: SearchService, private router: Router) {}
 
   ngOnInit() {
-    this.searchCtrl.valueChanges.subscribe(async x => {
+    this.searchCtrl.valueChanges.pipe(startWith('')).subscribe(async x => {
       this.searchResults = await this.searchService.searchResourcesAsync(x, 0, 5);
-    });
-
-    this.searchVisibilitySubscription = this.searchService.searchState.subscribe((state: SearchState) => {
-      this.showSearch = state.show;
     });
   }
 
@@ -42,7 +34,6 @@ export class ResourcesSearchComponent implements OnInit, AfterViewInit, OnDestro
   }
 
   optionSelected($event: MatAutocompleteSelectedEvent) {
-    // TODO open resource
     this.router.navigate(['../resources/results', { search_query: $event.option.value }]);
   }
 
@@ -51,15 +42,12 @@ export class ResourcesSearchComponent implements OnInit, AfterViewInit, OnDestro
     this.router.navigate(['../resources/results', { search_query: this.searchCtrl.value }]);
   }
 
-  ngOnDestroy() {
-    this.searchVisibilitySubscription.unsubscribe();
+  clearSearch() {
+    this.searchCtrl.setValue(null);
+    this.searchInput.nativeElement.focus();
   }
 
-  onStartSearch() {
-    this.searchService.show();
-  }
-
-  onStopSearch() {
-    this.searchService.hide();
+  toggleSearch() {
+    this.showSearch = !this.showSearch;
   }
 }
