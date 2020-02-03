62c31bddfe3d489470ec5e830ca57d7ec019f212
Emanuel Virca <emanuel.virca@gmail.com>
2020-02-03 20:56:55 +0200

    refactor: delete specs

--- src/app/web-portal/shared/pipes/convert-to-audio-resource/convert-to-audio-resource.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConvertToAudioResourcePipe } from './convert-to-audio-resource.pipe';
-
-describe('ConvertToAudioResourcePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ConvertToAudioResourcePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
